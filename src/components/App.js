9c4a563e8397f5f15a9d0469d25eee9a8fe9c8a9
ghostrib <noemail>
2019-02-21 20:24:48 -0600

    adds star ratings

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import StarRatings from 'react-star-ratings';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            included: [
+                'Samsung 65" Class - LED - NU6900 Series - 2160p - Smart - 4K UHD TV with HDR',
+                'Samsung TM1240A remote control',
+                'Stand'],
+        }
+    }
+
+    changeRating(newRating, name) {
+        this.setState({
+            rating: newRating
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Overview</h1>
+                <div>
+                    <h3>What's Included</h3>
+                    <hr />
+                    <ul>
+                        {this.state.included.map((items, i) => {
+                            return (
+                                <li key={i}>{items}</li>
+                            )
+                        })}
+                    </ul>
+
+                    <hr />
+                    <StarRatings
+                        rating={4.403}
+                        starDimension="20px"
+                        starSpacing="4px"
+                        starRatedColor="yellow"
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
